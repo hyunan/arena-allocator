10344ac9f060e76d7c9e9095c639e1b84b511931
Ray An <hsrayan05@gmail.com>
2026-06-19 22:37:28 -0700

    update const for methods

--- c++/include/arena.h
+++ c++/include/arena.h
@@ -13,12 +13,21 @@ public:
     Arena(std::size_t capacity);
     ~Arena(); // same as arena_free
 
+    Arena(const Arena& other) = delete;
+    Arena& operator=(const Arena& other) = delete;
+
+    Arena(Arena&& other) = delete;
+    Arena& operator=(Arena&& other) = delete;
+
     void* alloc(std::size_t size);
-    void* alloc_aligned(std::size_t size, std::size_t offset);
+    void* alloc_aligned(std::size_t size, std::size_t alignment);
     void reset();
 
-    std::size_t used(const Arena& arena);
-    std::size_t remaining(const Arena& arena);
+    std::size_t used() const;
+    std::size_t remaining() const;
+
+    std::size_t capacity() const;
+    std::size_t offset() const;
 };
 
 #endif
